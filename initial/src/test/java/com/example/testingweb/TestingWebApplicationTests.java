b5256e8447c6373733312724b562d3c88eaaee2e
Tr1ckC0 <trick.co@yahoo.com>
2021-12-17 22:49:45 -0600

    sanity check

--- initial/src/test/java/com/example/testingweb/TestingWebApplicationTests.java
+++ initial/src/test/java/com/example/testingweb/TestingWebApplicationTests.java
@@ -1,14 +1,21 @@
 package com.example.testingweb;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 public class TestingWebApplicationTests {
 
+	@Autowired
+	private HomeController controller;
+
 	@Test
 	public void contextLoads() {
+		assertThat(controller).isNotNull();
 	}
 
 }
